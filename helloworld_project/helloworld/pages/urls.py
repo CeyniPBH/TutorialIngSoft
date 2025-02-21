1eb56c3f0a7d8f897098161af247a8c99722f98b
CeyniPBH <cpbeltranh@eafit.edu.co>
2025-02-21 00:32:40 -0500

    segunda sección tutorial 1
    sección c

--- helloworld_project/helloworld/pages/urls.py
+++ helloworld_project/helloworld/pages/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import homePageView, AboutPageView, ContactPageView
+from .views import ProductIndexView, ProductShowView
 
 urlpatterns = [
-    path('', homePageView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name='about'),
-    path('contact/', ContactPageView.as_view(), name='contact'),
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path('products/<int:id>', ProductShowView.as_view(), name='show'),
 ]
